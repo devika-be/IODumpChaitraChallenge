bb65d6cd919234f81f5621f99a8e01045dff8a23
Devika Pagare <65743242+devika-be@users.noreply.github.com>
2023-04-02 18:36:53 +0530

    Create Day5.py

---
+++ Day5.py
@@ -0,0 +1,21 @@
+"""
+Day5: Program to print sum of n even numbers
+"""
+
+# Program
+
+num = int(input("Print sum of even numbers till: "))
+total = 0
+for i in range(1, num + 1):
+
+    if((i % 2) == 0):
+        total = total + i
+
+print("\nSum of even numbers from 1 to", num, "is", total)
+
+
+"""
+Input and Output:
+Print sum of even numbers till: 6
+Sum of even numbers from 1 to 6 is 12
+"""

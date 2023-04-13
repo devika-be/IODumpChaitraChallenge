313f5f0cf5cf3646e271f235418c47e96d8cf73a
Devika Pagare <65743242+devika-be@users.noreply.github.com>
2023-04-13 21:25:27 +0530

    Create Day13.py

---
+++ Day13.py
@@ -0,0 +1,20 @@
+""" 
+Day13: Write a program to find the Fibonacci series of n numbers using recursion.
+"""
+
+#Program
+
+def recur_fibo(n):
+   if n <= 1:
+       return n
+   else:
+       return(recur_fibo(n-1) + recur_fibo(n-2))
+
+nterms = 10
+
+if nterms <= 0:
+   print("Plese enter a positive integer")
+else:
+   print("Fibonacci sequence:")
+   for i in range(nterms):
+       print(recur_fibo(i))

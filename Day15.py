cc02b8a0cd0f76b700137530924037ca4adeeebc
Devika Pagare <65743242+devika-be@users.noreply.github.com>
2023-04-15 21:11:44 +0530

    Create Day15.py

---
+++ Day15.py
@@ -0,0 +1,19 @@
+""" 
+Day15 : Write a program to check whether the given number is palindrome or not.
+"""
+
+#Program
+
+my_str = 'aIbohPhoBiA'
+
+# make it suitable for caseless comparison
+my_str = my_str.casefold()
+
+# reverse the string
+rev_str = reversed(my_str)
+
+# check if the string is equal to its reverse
+if list(my_str) == list(rev_str):
+   print("The string is a palindrome.")
+else:
+   print("The string is not a palindrome.")

b67ae03a888b4ee491da7b680e957ac06eecc2ac
Devika Pagare <65743242+devika-be@users.noreply.github.com>
2023-04-26 10:37:14 +0530

    Create Day22.py

---
+++ Day22.py
@@ -0,0 +1,19 @@
+""" 
+Day22: Write a program to find first non-repeating character in a string.
+"""
+
+#Program
+
+string = "pythonprogram"
+index = -1
+fnc = ""
+for i in string:
+	if string.count(i) == 1:
+		fnc += i
+		break
+	else:
+		index += 1
+if index == 1:
+	print("Either all characters are repeating or string is empty")
+else:
+	print("First non-repeating character is", fnc)

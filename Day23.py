b556097d76e65cfc199cd89e216a9c2996f3d433
Devika Pagare <65743242+devika-be@users.noreply.github.com>
2023-04-28 20:01:31 +0530

    Create Day23.py

---
+++ Day23.py
@@ -0,0 +1,26 @@
+""" 
+Day23 : Implement a function that takes two integers as input and returns their greatest common divisor (GCD).
+"""
+
+#Program
+
+def gcd(a, b):
+
+	if (a == 0):
+		return b
+	if (b == 0):
+		return a
+
+	if (a == b):
+		return a
+
+	if (a > b):
+		return gcd(a-b, b)
+	return gcd(a, b-a)
+
+a = 98
+b = 56
+if(gcd(a, b)):
+	print('GCD of', a, 'and', b, 'is', gcd(a, b))
+else:
+	print('not found')

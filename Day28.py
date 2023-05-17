3c50161a5b63fea12fcfd6c29c29b231ccd4f5d0
Devika Pagare <65743242+devika-be@users.noreply.github.com>
2023-05-17 17:30:39 +0530

    Create Day28.py

---
+++ Day28.py
@@ -0,0 +1,11 @@
+""" 
+Day28: Implement a function that takes a list of strings as input and returns a new list that contains all the strings with the first letter capitalized.
+"""
+
+#Program
+
+out = map(lambda x:x.lower(), ['DevIKA', 'Raj', 'SakSHI'])
+
+output = list(out)
+
+print(output)
